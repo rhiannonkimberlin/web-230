fc4e98a0e068050437408409d95e8f3822c5d4e9
Your Name <you@example.com>
2020-05-31 10:08:06 -0700

    Adding assignment 1.4.js

---
+++ week-1/assignment_4.js
@@ -0,0 +1,9 @@
+var firstName = "Rhiannon";
+var lastName = "Kimberlin";
+var age = 28;
+var street = "1111 w Villa Linda Rd";
+var city = "Peoria";
+var zipCode = 85027;
+
+console.log(firstName, lastName, age)
+console.log(street, city, zipCode)
