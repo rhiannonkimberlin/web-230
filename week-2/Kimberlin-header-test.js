94b02b7c1d2a88e40bd186cb815909c53b019f2c
rhiannonkimberlin <r.kim@live.com>
2020-06-07 08:07:52 -0700

    Exercise 2.2

---
+++ week-2/Kimberlin-header-test.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  kimberlin-header-test.js
+; Author: Rhiannon Kimberlin
+; Date:   7 June 2020
+; Modified By: Rhiannon Kimberlin
+; Description: Part 3 of Header Instructions
+;===========================================
+*/
+
+/*Import Statement*/
+const header = require ('./Kimberlin-header.js');
+
+/*Three String Values*/
+console.log(header.display('Rhiannon', 'Kimberlin', 'Exercise 2.2'));
+
+/*Welcome Message - didnt see in instructions but wanted to match output*/
+console.log('Welcome to exercise 2.2')
