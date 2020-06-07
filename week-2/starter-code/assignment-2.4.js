3086f68649fe305bbace819ddc59dddac1ad639c
rhiannonkimberlin <r.kim@live.com>
2020-06-07 09:42:46 -0700

    Added Exercise 2.4.js

--- week-2/starter-code/assignment-2.4.js
+++ week-2/starter-code/assignment-2.4.js
@@ -1,15 +1,49 @@
 /*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
-
-  FirstName LastName
-  Assignment 2.4
-  Today's Date
-
-  Hello my name is <your actual name goes here>!
-
   Today's date is <today's date> and the current temperature is <33.6 - use the formatNumber function> degrees.
 
   I am <your actual age - parsed int value> years old and my savings account goal is <your actual goal - parsed float value> dollars.
 
 */
+
+/*
+============================================
+; Title:  kimberlin-assignment-2.4.js
+; Author: Rhiannon Kimberlin
+; Date:   7 June 2020
+; Modified By: Rhiannon Kimberlin
+; Description: exercise 2.4
+;===========================================
+*/
+
+/*Define myName*/
+myName.rhiannon = "Rhiannon" + " " + "Kimberlin";
+
+/*Step 1 Function*/
+function myName() {
+  return myName.rhiannon ;
+}
+
+/*define dateWriter*/
+dateWriter.date = "June" + " " + 7 + "," + 2020;
+
+/*Step 2 Function*/
+function dateWriter(){
+  return dateWriter.date;
+}
+
+/*define formatNumber*/
+formatNumber.temperature
+
+/*display assignment information*/
+console.log(myName());
+console.log('Assignment 2.4');
+console.log(dateWriter());
+
+/*display hello + my name is*/
+console.log('Hello my name is', myName(), "!");
+
+/*Display sentence about date and temperature*/
+console.log("Today's date is ", dateWriter(), " and the current temperature is ", formatNumber(), " degrees.");
+
+/*Display age sentence*/
+console.log("I am ",  , " years old and my savings account goal is ",  , " dollars.")
--- week-2/starter-code/exercise-2.3.js
+++ week-2/starter-code/exercise-2.3.js
@@ -10,3 +10,24 @@
   Hint: Use your personal header display function and
   refer to page 178 for implementing function properties
 */
+
+/*
+============================================
+; Title:  exercise-2.3.js
+; Author: Professor Krasso 
+; Date:   7 June 2020
+; Modified By: Rhiannon Kimberlin
+; Description: Output Described Above
+;===========================================
+*/
+
+/*Define myName*/
+myName.rhiannon = "Rhiannon" + " " + "Kimberlin";
+
+/*Function for my name*/
+function myName() {
+  return myName.rhiannon ;
+}
+
+/*display hello + my name*/
+console.log('Hello', myName()), "!";
